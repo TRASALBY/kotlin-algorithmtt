8340ebc0f3cb41359f60c83a93af31073fc48344
TRASALBY <54586491+TRASALBY@users.noreply.github.com>
2023-05-02 00:30:33 +0900

    [Gold III] Title: 개미굴, Time: 336 ms, Memory: 27140 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.kt
+++ 백준/Gold/14725. 개미굴/개미굴.kt
@@ -5,34 +5,35 @@ import java.util.*
 private fun main() {
     val n = readln().toInt()
 
-    val root = Node()
+    val trie = Trie()
     repeat(n) {
         val input = readln().split(" ")
-        val k = input.first()
-        var node = root
-        for (i in 1..input.lastIndex) {
-            node = node.leaf.computeIfAbsent(input[i]) {
-                Node()
-            }
-        }
+        trie.insert(input.subList(1,input.size))
     }
     val sb = StringBuilder()
-    root.print(sb, 0)
 
+    trie.print(sb, 0)
     println(sb)
 
 }
 
-class Node {
-    val leaf = TreeMap<String, Node>()
+class Trie(val node: TreeMap<String, Trie> = TreeMap()){
+    fun insert(foods: List<String>) {
+        var child: Trie = this
+        foods.forEach{
+            child = child.node.computeIfAbsent(it){
+                Trie(TreeMap())
+            }
+        }
+    }
 
-    fun print(sb: StringBuilder, depth: Int) {
-        for (k in leaf.keys) {
-            for (i in 0 until depth) {
+    fun print(sb: StringBuilder, depth: Int){
+        for(key in node.keys){
+            for(i in 0 until depth){
                 sb.append("--")
             }
-            sb.appendLine(k)
-            leaf[k]?.print(sb, depth + 1)
+            sb.appendLine(key)
+            node[key]?.print(sb, depth+1)
         }
     }
 }
