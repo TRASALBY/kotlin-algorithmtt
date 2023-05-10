ea398f021915d4f30ddefb044a22c8d936176d46
TRASALBY <54586491+TRASALBY@users.noreply.github.com>
2023-05-10 22:28:56 +0900

    [Silver I] Title: 쿼드트리, Time: 160 ms, Memory: 116432 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,81 +1,31 @@
 import sys
+
 input = sys.stdin.readline
 
-global answer
-answer = ''
-def quad(arr):
-    global answer
-
-    n = len(arr)//2
-
-    arr1 = []
-    for i in range(n):
-        arr1.append(arr[i][:n])
-    s = sum(sum(arr1,[]))
-
-    if s == len(arr1) ** 2:
-        answer += '1'
-    elif s == 0:
-        answer += '0'
-    else:
-        answer += '('
-        quad(arr1)
-        answer += ')'
-
-    arr2 = []
-    for i in range(n):
-        arr2.append(arr[i][n:])
-    s = sum(sum(arr2,[]))
-    if s == len(arr2) ** 2:
-        answer += '1'
-    elif s == 0:
-        answer += '0'
-    else:
-        answer += '('
-        quad(arr2)
-        answer += ')'
-
-    arr3 = []
-    for i in range(n,2*n):
-        arr3.append(arr[i][:n])
-    s = sum(sum(arr3,[]))
-    if s == len(arr3) ** 2:
-        answer += '1'
-    elif s == 0:
-        answer += '0'
-    else:
-        answer += '('
-        quad(arr3)
-        answer += ')'
-
-    arr4 = []
-    for i in range(n,2*n):
-        arr4.append(arr[i][n:])
-    s = sum(sum(arr4,[]))
-    if s == len(arr4) ** 2:
-        answer += '1'
-    elif s == 0:
-        answer += '0'
-    else:
-        answer += '('
-        quad(arr4)
-        answer += ')'
-
-
 n = int(input())
-graph = []
+map = []
+
+
+def printRecursion(x, y, n):
+    now = map[y][x]
+    global answer
+    for nx in range(x, x + n):
+        for ny in range(y, y + n):
+            if map[ny][nx] != now:
+                answer += "("
+                printRecursion(x, y, n // 2)
+                printRecursion(x + n // 2, y, n // 2)
+                printRecursion(x, y + n // 2, n // 2)
+                printRecursion(x + n // 2, y + n // 2, n // 2)
+                answer += ")"
+                return
+    answer += now
+
+
 for i in range(n):
-    graph.append(list(map(int,list(str(input().strip())))))
+    map.append((input().strip()))
 
-s = sum(sum(graph,[]))
-if s == len(graph) ** 2:
-    answer += '1'
-elif s == 0:
-    answer += '0'
-else:
-    answer += '('
-    quad(graph)
-    answer += ')'
+answer = ""
+printRecursion(0, 0, n)
+
 print(answer)
-
-
