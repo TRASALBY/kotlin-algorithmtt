fb9de4c67684593bf8c181e378340eaa1c045ca9
TRASALBY <54586491+TRASALBY@users.noreply.github.com>
2023-06-20 23:51:47 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 208 ms, Memory: 16792 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.kt
@@ -0,0 +1,34 @@
+package `kotlin-algorithm`.`0x14_TwoPointer`
+
+import java.io.StreamTokenizer
+
+fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun input(): Int {
+        nextToken()
+        return nval.toInt()
+    }
+    val n = input()
+    val k = input()
+    
+    val arr = IntArray(n) {
+        input()
+    }
+    val visited = IntArray(100001)
+
+    var left = 0
+    var right = 0
+
+    var answer = 0
+
+    while (right < n) {
+        if (visited[arr[right]] < k) {
+            visited[arr[right]]++
+            right++
+        } else {
+            visited[arr[left]]--
+            left++
+        }
+        answer = maxOf(answer, right - left)
+    }
+    println(answer)
+}
