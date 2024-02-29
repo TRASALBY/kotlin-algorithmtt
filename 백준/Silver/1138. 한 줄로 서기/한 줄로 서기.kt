ab536b7a7ab403aad616aac455085f0902cb02d3
TRASALBY <54586491+TRASALBY@users.noreply.github.com>
2024-03-01 00:18:44 +0900

    [Silver II] Title: 한 줄로 서기, Time: 160 ms, Memory: 23984 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val n = br.readLine().toInt()
+
+    val arr = br.readLine().split(" ").map { it.toInt() }
+
+    val answer = IntArray(n)
+
+    for (i in 0 until n) {
+        var cnt = 0
+        for (j in 0 until n) {
+            if (answer[j] == 0) {
+                if (cnt == arr[i]) {
+                    if (answer[j] == 0) {
+                        answer[j] = i + 1
+                        break
+                    }
+                } else {
+                    cnt++
+                }
+            }
+        }
+    }
+    println(answer.joinToString(" "))
+
+}
